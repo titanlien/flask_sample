e81a7453f87f92d842bad3317309459166fcae59
Titan @MBA <titan.lien@gmail.com>
2015-11-21 17:35:40 +0800

    add dashboard and like, part 7

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def validateLogin():
         if len(data) > 0:
             if check_password_hash(str(data[0][3]),_password):
                 session['user'] = data[0][0]
-                return redirect('/userHome')
+                return redirect('/showDashboard')
             else:
                 return render_template('error.html',error = 'Wrong Email address or Password.')
         else:
@@ -180,6 +180,35 @@ def getWish():
     except Exception as e:
         return render_template('error.html', error = str(e))
 
+@app.route('/getAllWishes')
+def getAllWishes():
+    try:
+        if session.get('user'):
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_GetAllWishes')
+            result = cursor.fetchall()
+
+
+
+            wishes_dict = []
+            for wish in result:
+                wish_dict = {
+                        'Id': wish[0],
+                        'Title': wish[1],
+                        'Description': wish[2],
+                        'FilePath': wish[3]}
+                wishes_dict.append(wish_dict)
+
+
+
+            return json.dumps(wishes_dict)
+        else:
+            return render_template('error.html', error = 'Unauthorized Access')
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+
 @app.route('/getWishById',methods=['POST'])
 def getWishById():
     try:
@@ -266,6 +295,38 @@ def upload():
     	f_name = str(uuid.uuid4()) + extension
     	file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
         return json.dumps({'filename':f_name})
+
+@app.route('/showDashboard')
+def showDashboard():
+    return render_template('dashboard.html')
+
+@app.route('/addUpdateLike',methods=['POST'])
+def addUpdateLike():
+    try:
+        if session.get('user'):
+            _wishId = request.form['wish']
+            _like = request.form['like']
+            _user = session.get('user')
+
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_AddUpdateLikes',(_wishId,_user,_like))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'status':'OK'})
+            else:
+                return render_template('error.html',error = 'An error occurred!')
+
+        else:
+            return render_template('error.html',error = 'Unauthorized Access')
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    finally:
+        cursor.close()
+        conn.close()
 
 if __name__ == "__main__":
     app.run()
